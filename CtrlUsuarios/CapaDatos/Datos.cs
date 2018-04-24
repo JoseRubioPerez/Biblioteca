8530e6d8c18950b22eeb6fd23c750d5be4e582b2
Jose Luis Rubio <joshsk8rocker@gmail.com>
2018-04-24 12:26:48 -0600

    1. Se cambió el proyecto [CentroDeInformación] por [CtrlUsuarios]. Son lo mismo pero con diferente código. 2. Se cambiaron los formularios secundarios por controles de usuarios. 3. Se modificó el archivo para insertar en la base de datos.
    IMPORTANTE: Cualquiera que quiera descargar la solución del proyecto, no olvide cambiar el archivo [App.config].

---
+++ CtrlUsuarios/CapaDatos/Datos.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CapaDatos
+{
+    public class Datos
+    {
+        private SqlConnection conex; private SqlCommand cmd; private SqlDataAdapter da; private DataTable dt;
+
+        public Datos()
+        {
+            try { conex = new SqlConnection(ConfigurationManager.ConnectionStrings["biblioteca"].ConnectionString); }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error de conexión a base de datos: " + e.Message + "\n1. Verifique que el archivo \"App.config\" de la Capa de " +
+                    "Presentación (CapaPre), tenga la cadena de conexión correcta y apunte a su instancia SQL (línea de código 6 a la 8).\n2. Asegurese de tener el nombre de la base de datos como \"pBiblioteca\".");
+            }
+        }
+
+        public DataTable Procedimiento(string NameProcedure, List<object> ValorTexto, List<string> NombreParametro, List<byte> idDato)
+        {
+            try
+            {
+                cmd = new SqlCommand(NameProcedure, conex);
+                cmd.CommandType = CommandType.StoredProcedure;
+                SqlParameter[] metodo = new SqlParameter[ValorTexto.Count];
+                for (int i = 0; i < metodo.Length; i++)
+                {
+                    metodo[i] = new SqlParameter();
+                    switch (idDato[i])
+                    {
+                        case 1: metodo[i].SqlDbType = SqlDbType.VarChar; break;
+                        case 2: metodo[i].SqlDbType = SqlDbType.Char; break;
+                        case 3: case 4: case 5: metodo[i].SqlDbType = SqlDbType.Int; break;
+                        case 6: metodo[i].SqlDbType = SqlDbType.Date; break;
+                        case 7: metodo[i].SqlDbType = SqlDbType.Time; break;
+                        case 8: metodo[i].SqlDbType = SqlDbType.Image; break;
+                    }
+                    cmd.Parameters.AddWithValue(NombreParametro[i], ValorTexto[i]);
+                }
+                conex.Open();
+                cmd.ExecuteNonQuery();
+                conex.Close();
+                da = new SqlDataAdapter(cmd);
+                dt = new DataTable();
+                da.Fill(dt);
+                Console.WriteLine("Tarea: \"Procedimiento\" realizada con éxito.");
+            }
+            catch (Exception e) { Console.WriteLine("Error: " + e.Message); }
+            finally { conex.Close(); }
+            return dt;
+        }
+    }
+}
---
+++ CtrlUsuarios/CapaPre/Main.cs
@@ -0,0 +1,171 @@
+﻿using CapaNegocio;
+using CapaEntidad;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CapaPre
+{
+    internal enum TipoIcono { Danger = 1, Warning = 2, Info = 3 }
+
+    public partial class Main : Form
+    {
+
+        #region Instancias
+
+        private Entidad entidad = new Entidad();
+        private Negocio negocio = new Negocio();
+
+        #endregion Instancias
+
+        private Control[] arreglo;
+        private byte menu;
+
+        public Main()
+        {
+            InitializeComponent();
+            this.Size = new Size(Screen.PrimaryScreen.WorkingArea.Width, Screen.PrimaryScreen.WorkingArea.Height);
+            MinimumSize = new Size(Screen.PrimaryScreen.WorkingArea.Width, Screen.PrimaryScreen.WorkingArea.Height);
+            OcultarControlUsers();
+        }
+
+        public Main(string user, string password, char supersu)
+        {
+            InitializeComponent();
+            this.Size = new Size(Screen.PrimaryScreen.WorkingArea.Width, Screen.PrimaryScreen.WorkingArea.Height);
+            MinimumSize = new Size(Screen.PrimaryScreen.WorkingArea.Width, Screen.PrimaryScreen.WorkingArea.Height);
+            //Entidad
+            entidad.setAdminNC(user);
+            entidad.setAdminPassword(password);
+            entidad.setAdminSuperSu(supersu);
+            //Main -> myPassword1
+            myPassword1.Admin[0] = user;
+            myPassword1.Admin[1] = password;
+            myPassword1.supersu = supersu;
+            lbTitulo.Text = "Sistema de Control de Usuarios. Bienvenido: " + entidad.getAdminNC();
+            OcultarControlUsers();
+        }
+
+        public void OcultarControlUsers()
+        {
+            UserControl[] ctlUser = new UserControl[] { myPassword1, othersPasswords1 };
+            for(int i = 0; i < ctlUser.Length; i++)
+                ctlUser[i].Visible = false;
+        }
+
+        public void MostrarForm(Control ctlUser, DockStyle Estilo)
+        {
+            panelContenido.Controls.Clear();
+            //ctlUser.TopLevelControl = false;
+            ctlUser.Parent = panelContenido;
+            ctlUser.Dock = Estilo;
+            ctlUser.Show();
+        }
+
+        public void MostrarCtrl(UserControl ctlUser, DockStyle Estilo)
+        {
+            if (ctlUser.Visible)
+                ctlUser.BringToFront();
+            else
+                ctlUser.Show();
+            ctlUser.Parent = panelContenido;
+            ctlUser.Dock = Estilo;
+        }
+
+        private void EventoClick(object sender, EventArgs e)
+        {
+            arreglo = new Control[] { pictureClose, pictureMinimize };
+            for (; menu < arreglo.Length; menu++)
+            {
+                if (arreglo[menu] == sender)
+                    break;
+            }
+            switch (menu)
+            {
+                case 0:
+                    Question pregunta = new Question((byte)TipoIcono.Warning, "Salir", "¿Deseas cerrar el programa?", "Sí aceptas, se cerrará tu sesión actual.", true);
+                    DialogResult dr = pregunta.ShowDialog();
+                    if (dr == DialogResult.Yes)
+                    {
+                        LogOut logout = new LogOut(entidad.getAdminNC(), entidad.getAdminPassword());
+                        logout.ShowDialog();
+                        if (logout.ConfirmacionCerrar())
+                            Application.Exit();
+                    }
+                    break;
+
+                case 1: WindowState = (WindowState == FormWindowState.Normal) ? WindowState = FormWindowState.Minimized : WindowState = FormWindowState.Normal; break;
+            }
+            menu = 0;
+        }
+
+        private void EventoClickMenuItem(object sender, EventArgs e)
+        {
+            byte item = 0;
+            ToolStripMenuItem[] menuitem = new ToolStripMenuItem[] { usuariosMenu, ImportarUsuariosMenuItem, AgregarAdminMenuItem, CambiarMiContraMenuItem, CambiarOtraContraMenuItem };
+            for (; item < menuitem.Length; item++)
+            {
+                if (menuitem[item] == sender)
+                    break;
+            }
+            switch (item)
+            {
+                case 0:
+                    break;
+
+                case 3:
+                    MostrarCtrl(myPassword1, DockStyle.Fill);
+                    break;
+                case 4:
+                    MostrarCtrl(othersPasswords1, DockStyle.Fill);
+                    break;
+            }
+            item = 0;
+        }
+
+        private void CambiarColorMenuItem(object sender, EventArgs e)
+        {
+            byte item = 0;
+            ToolStripMenuItem[] menuitem = new ToolStripMenuItem[] { usuariosMenu, reportesMenu, configuracionMenu };
+            for (; item < menuitem.Length; item++)
+            {
+                if (menuitem[item] == sender)
+                    break;
+            }
+            switch (item)
+            {
+                case 0: usuariosMenu.ForeColor = Color.FromArgb(19, 27, 35); break;
+                case 1: reportesMenu.ForeColor = Color.FromArgb(19, 27, 35); break;
+                case 2: configuracionMenu.ForeColor = Color.FromArgb(19, 27, 35); break;
+            }
+            item = 0;
+        }
+
+        private void ResetColorMenuItem(object sender, EventArgs e)
+        {
+            byte item = 0;
+            ToolStripMenuItem[] menuitem = new ToolStripMenuItem[] { usuariosMenu, reportesMenu, configuracionMenu };
+            for (; item < menuitem.Length; item++)
+            {
+                if (menuitem[item] == sender)
+                    break;
+            }
+            switch (item)
+            {
+                case 0: usuariosMenu.ForeColor = Color.FromArgb(231, 223, 198); break;
+                case 1: reportesMenu.ForeColor = Color.FromArgb(231, 223, 198); break;
+                case 2: configuracionMenu.ForeColor = Color.FromArgb(231, 223, 198); break;
+            }
+            item = 0;
+        }
+
+        private void CancelarF4(object sender, FormClosingEventArgs e)
+        {
+            switch (e.CloseReason)
+            {
+                case CloseReason.UserClosing: e.Cancel = true; break;
+                case CloseReason.ApplicationExitCall: e.Cancel = false; break;
+            }
+        }
+    }
+}
